caed17470027c73e27e6cc031560d4a8a659b130
srikanth <srikanth@srikanth-linux>
2017-03-18 23:18:04 -0700

    Add two numbers, Longest non repeating substring

--- leetcode_test.c
+++ leetcode_test.c
@@ -8,26 +8,139 @@
 #include<stdlib.h>
 #include<string.h>
 
+typedef struct ns {
+	int val;
+	int idx;
+}nt;
 
+struct ListNode {
+	int val;
+	struct ListNode *next;
+};
+
+
+typedef struct ListNode ListNode_t;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+#define MAX_CHARS 255
+/**
+ * Length of the longest substring of
+ * non-repeating characters.
+ */
+int lengthOfLongestSubstring(char* s) {
+	int hs[MAX_CHARS];
+	int max = 0, i, start = -1;
+
+
+	for(i = 0; i < MAX_CHARS; i++){
+		hs[i] = -1;
+	}
+
+	for(i = 0; s[i] != '\0'; i++) {
+		if (hs[(int)s[i]] > start) {
+			start = hs[(int)s[i]];
+		}
+
+		hs[(int)s[i]] = i;
+		if (max < (i -  start)) {
+			max = i - start;
+		}
+	}
+
+	return max;
+}
+
+
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
+{
+	ListNode_t *curr;
+	ListNode_t dummy;
+	int sum = 0;
+
+	curr = &dummy;
+
+
+	while (l1 || l2) {
+		curr->next = (ListNode_t*)calloc(1, sizeof(ListNode_t));
+		curr = curr->next;
+        sum /= 10;
+
+        if (l1) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+
+        if (l2) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+
+		curr->val = sum % 10;
+	}
+
+	if (sum / 10) {
+		curr->next = (ListNode_t*)calloc(1, sizeof(ListNode_t));
+		curr->next->val = sum/10;
+	}
+
+	return dummy.next;
+}
+
+void quick_sort_nums(nt* nums, int l, int h)
+{
+	int i, j, pivot;
+	nt temp;
+
+	if ((l >= h) || (l < 0) || (h < 0)) {
+		return;
+	}
+
+	pivot = h;
+	i = j = l;
+
+
+	while (j < pivot) {
+		if (nums[j].val < nums[pivot].val) {
+			temp = nums[j];
+			nums[j] = nums[i];
+			nums[i] = temp;
+			i++;
+		}
+		j++;
+	}
+
+	temp = nums[pivot];
+	nums[pivot] = nums[i];
+	nums[i] = temp;
+
+	quick_sort_nums(nums, l, i -1);
+	quick_sort_nums(nums, i + 1, h);
+
+	return;
+}
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* twoSum(int* nums, int numsSize, int target) {
-	struct ns {
-		int val;
-		int idx;
-	};
 
 	int *result_nums;
 	int i, j;
-	struct ns *sorted_nums;
+	nt *sorted_nums;
 
 
 	if (!nums || (numsSize < 0) ) {
 		return NULL;
 	}
 
-	sorted_nums = (struct ns *)malloc(sizeof(struct ns) * numsSize);
+	sorted_nums = (nt *)malloc(sizeof(nt) * numsSize);
 	if (!sorted_nums) {
 		return NULL;
 	}
@@ -37,15 +150,18 @@ int* twoSum(int* nums, int numsSize, int target) {
 		sorted_nums[i].idx = i;
 	}
 
+#if 0
 	for (i = 1; i < numsSize; i++) {
 		for (j = 0; j < (numsSize - i); j++) {
 			if (sorted_nums[j].val > sorted_nums[j+1].val) {
-				struct ns temp = sorted_nums[j];
+				nt temp = sorted_nums[j];
 				sorted_nums[j] = sorted_nums[j+1];
 				sorted_nums[j+1] = temp;
 			}
 		}
 	}
+#endif
+	quick_sort_nums(sorted_nums, 0, (numsSize-1));
 
 	printf("Sorted [");
 	for(i = 0; i < numsSize; i++) {
@@ -92,7 +208,7 @@ error:
 
 void test_twoSum(void)
 {
-	int arr[4] = {15, 7, 11, 2};
+	int arr[4] = {2, 7, 11, 4};
 	int numsSize = 4;
 	int *result;
 
@@ -106,8 +222,24 @@ void test_twoSum(void)
 	return;
 }
 
+void test_LongestSubstring(void)
+{
+	char string[512] = "dvdf";
+	int len;
+
+	len = lengthOfLongestSubstring(string);
+
+	printf("Len : %d\n", len);
+
+	return;
+}
+
+
+
 int main(void)
 {
-	test_twoSum();
+	//test_twoSum();
+	test_LongestSubstring();
+	printf("Done\n");
 	return 0;
 }
